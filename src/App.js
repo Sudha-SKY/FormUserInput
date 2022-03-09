f106f02dbc0d6e8210f761e1c8199c998084b975
Sudha-SKY <96175627+Sudha-SKY@users.noreply.github.com>
2022-03-09 10:00:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import BasicForm from "./components/BasicForm";
+
+function App() {
+  return (
+    <div className="app">
+      <BasicForm />
+    </div>
+  );
+}
+
+export default App;
+
+// import SimpleInput from './components/SimpleInput';
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <SimpleInput />
+//     </div>
+//   );
+// }
+
+// export default App;
